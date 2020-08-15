215a47692d115d5d460320a5ddb744fb4f995b2a
shiju <shiju@LAPTOP-QFQM3362>
2020-08-15 11:05:00 +0530

    Commit

---
+++ hotelbooking/src/main/java/project/selfefd/dto/UserDTO.java
@@ -0,0 +1,5 @@
+package project.selfefd.dto;
+
+public class UserDTO {
+
+}
---
+++ hotelbooking/src/main/java/project/selfefd/restcontroller/LoginController.java
@@ -0,0 +1,27 @@
+package project.selfefd.restcontroller;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+import project.selfefd.dto.UserDTO;
+
+@RestController
+public class LoginController {
+	
+
+    public ResponseEntity<UserDTO> getCountry() {
+    	
+    	UserDTO user = new UserDTO();
+        
+        
+    	HttpHeaders headers = new HttpHeaders();
+        headers.add("Responded", "MyController");
+        
+        return ResponseEntity.accepted().headers(headers).body(user);
+
+        
+
+    }
+	
+}
